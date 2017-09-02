d1ad27dd5f640c35944ff5f3e4c418e6c6754f62
Joseph Sungpil Choi <chousemath@gmail.com>
2017-09-02 18:24:42 +0900

    added some points

--- map.js
+++ map.js
@@ -1,8 +1,27 @@
 var mymap = L.map('mapid').setView([34.6937378, 135.50216509999996], 13);
 // pk.eyJ1Ijoiam9jaG9pMDcwNyIsImEiOiJjajczMWZrcTkwNHo2MzNvNGIzOHI3MW85In0.GSHlVF_HltDmEvomI2m_CA
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1Ijoiam9jaG9pMDcwNyIsImEiOiJjajczMWZrcTkwNHo2MzNvNGIzOHI3MW85In0.GSHlVF_HltDmEvomI2m_CA'
 }).addTo(mymap);
+var markerArray = [
+    {
+        title: 'Port of Osaka',
+        description: 'Formerly called the Naniwazu Port, the Port of Osaka was the ancient entry point for visitors from other parts of Asia.',
+        lat: 34.61405945,
+        lon: 135.4403915
+    },
+    {
+        title: 'Shitennō-ji (四天王寺)',
+        description: 'Possibly the first Buddhist temple in Japan.',
+        lat: 34.6539,
+        lon: 135.51645
+    }
+];
+for (var i = 0; i < markerArray.length; i++) {
+    var marker = L.marker([markerArray[i].lat, markerArray[i].lon]).addTo(mymap);
+    marker.bindPopup("<span class=\"marker-title\">" + markerArray[i].title + "</span>");
+    if (i === markerArray.length - 1)
+        marker.openPopup();
+}
